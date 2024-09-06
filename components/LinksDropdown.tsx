55b7eab71ce44aeaad32e61e4acd19218ff9cd92
odik91 <ali.shoddiqien@gmail.com>
2024-09-06 21:47:15 +0700

    adding mobile sidebar menu

--- components/LinksDropdown.tsx
+++ components/LinksDropdown.tsx
@@ -1,6 +1,39 @@
+import { AlignLeft } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import links from "@/utils/links";
+import Link from "next/link";
+
 const LinksDropdown = () => {
   return (
-    <div>LinksDropdown</div>
-  )
-}
-export default LinksDropdown
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          const { href, icon, label } = link;
+          return (
+            <DropdownMenuItem key={href}>
+              <Link href={href} className="flex items-center gap-x-2">
+                {icon} <span className="capitalize">{label}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default LinksDropdown;
