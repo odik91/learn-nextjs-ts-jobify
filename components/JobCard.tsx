75ce27864781197eb139c0f0c734f29e7e4e4e21
odik91 <ali.shoddiqien@gmail.com>
2024-09-09 23:24:40 +0700

    initialize lists job

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,6 +1,38 @@
 import { JobType } from "@/utils/types";
+import { Briefcase, CalendarDays, MapPin, RadioTower } from "lucide-react";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import JobInfo from "./JobInfo";
+import DeleteJobBtn from "./DeleteJobBtn";
 
 const JobCard = ({ job }: { job: JobType }) => {
-  return <div>JobCard</div>;
+  const { createdAt, position, company, id } = job;
+  const date = new Date(createdAt).toLocaleDateString();
+
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{position}</CardTitle>
+        <CardDescription>{company}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent>{/* card indo */}</CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/jobs/${job.id}`}>Edit</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
 };
 export default JobCard;
--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -1,6 +1,33 @@
+"use client";
+import { getAllJobsAction } from "@/utils/action";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import JobCard from "./JobCard";
+
 const JobsList = () => {
-  return (
-    <div>JobsList</div>
-  )
-}
-export default JobsList
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const pageNumber = Number(searchParams.get("page")) || 1;
+
+  const { data, isPending } = useQuery({
+    queryKey: ["jobs", search ?? '', jobStatus, pageNumber],
+    queryFn: () => getAllJobsAction({ search, jobStatus, page: pageNumber }),
+  });
+
+  const jobs = data?.jobs || [];
+
+  if (isPending) return <h2 className="text-xl">Please wait...</h2>
+
+  if (jobs.length < 1) return <h2 className="text-xl">No jobs found</h2>
+
+  return <>
+  <div className="grid md:grid-cols-2 gap-8">
+    {jobs.map((job) => {
+      return <JobCard key={job.id} job={job} />
+    })}
+  </div>
+  </>;
+};
+export default JobsList;
