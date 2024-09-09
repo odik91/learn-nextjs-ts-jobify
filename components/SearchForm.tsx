4075ce082782ecbd60cebccc0bf0a786de43e591
odik91 <ali.shoddiqien@gmail.com>
2024-09-09 20:44:50 +0700

    adding search form functionality

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,6 +1,64 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Input } from "./ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { JobStatus } from "@/utils/types";
+import { Button } from "./ui/button";
+
 const SearchForm = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const params = new URLSearchParams();
+
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
   return (
-    <div>SearchForm</div>
-  )
-}
-export default SearchForm
+    <form
+      className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <Input
+        type="text"
+        placeholder="Search Job"
+        name="search"
+        defaultValue={search}
+      />
+      <Select defaultValue={jobStatus} name="jobStatus">
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {["all", ...Object.values(JobStatus)].map((jobStatus) => {
+            return (
+              <SelectItem key={jobStatus} value={jobStatus}>
+                {jobStatus}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+      <Button type="submit">Search</Button>
+    </form>
+  );
+};
+export default SearchForm;
